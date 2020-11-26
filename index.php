4d69c5ca8e600db4dd71d29733ed657a900603ed
Aldo Junior <aldocfjunior@gmail.com>
2020-11-26 19:34:55 -0300

    iniciando

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="pt-br" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="pt-br" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="pt-br">
+<!--<![endif]-->
+<!-- HEAD -->
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport"/>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+
+        <link href="./src/plugins/bootstrap-4.5.3-dist/css/bootstrap.css" rel="stylesheet" type="text/css" />
+        <link href="./src/css/formCep.css" rel="stylesheet" type="text/css" />
+        
+    </head>
+
+    <body>
+
+        <?php 
+            include('./contents/formCep.php');
+        ?>
+
+        <script src="./src/plugins/jquery-3.5.1.js" type="text/javascript"></script>
+        <script src="./src/plugins/bootstrap-4.5.3-dist/js/bootstrap.js" type="text/javascript"></script>
+        <script src="./src/js/formCep.js" type="text/javascript"></script>
+
+    </body>
+</html>
